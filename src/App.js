5cffe26c3dd8be621d1873addc9a9dac0c10e840
paoloBugarin <paolopaolobugarin@gmail.com>
2022-11-06 14:46:16 -0800

    created CartPage, created Modal on ProductPage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomePage from "./pages/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import ProductPage from "./pages/ProductPage";
 import useProductsRedux from "./hooks/useProductsRedux";
+import CartPage from "./pages/CartPage";
 
 const App = () => {
   const { fetchProducts } = useProductsRedux();
@@ -20,6 +21,7 @@ const App = () => {
           <Routes>
             <Route element={<HomePage />} path="/"/>
             <Route element={<ProductPage />} path="/product/:id"/>  
+            <Route element={<CartPage />} path="/cart"/>  
           </Routes>
         </Container>
       </main>
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,15 +1,24 @@
-import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, ListGroupItem } from 'react-bootstrap'
+import { useState } from 'react'
+import { Link, useParams, useNavigate } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, ListGroupItem, FormControl, Modal, ModalHeader, CloseButton, ModalTitle, ModalBody, ModalFooter } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import useProductsRedux from '../hooks/useProductsRedux'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
 const ProductPage = () => {
     const params = useParams();
+    const navigate = useNavigate();
     const { productsList: { error, isLoading, products } } = useProductsRedux();
     const product = products && products.find(item => item._id === params.id);
-    
+    const [ quantity, setQuantity ] = useState(1);  //default quantity is 1
+    const [ showModal, setShowModal ] = useState(false);
+    const addToCartHandler = () => {
+        setShowModal(true)
+        // navigate("/cart")
+    }
   return (
     <>
         <Link className='btn btn-light my-3' to="/">Go Back</Link>
@@ -40,13 +49,45 @@ const ProductPage = () => {
                                 <Col>{product.countInStock > 0 ? "In Stock" : "Out of Stock"}</Col>
                             </Row>
                         </ListGroupItem>
+                            {product.countInStock > 0 && (
+                                <ListGroupItem>
+                                    <Row>
+                                        <Col>Qty:</Col>
+                                        <Col>
+                                            <FormControl as="select" value={quantity} onChange={e => setQuantity(e.target.value)}>
+                                                {[...Array(product.countInStock).keys()].map(item => (
+                                                    <option key={item + 1} value={item + 1}>{item + 1}</option>
+                                                ))}
+                                            </FormControl>
+                                        </Col>
+                                    </Row>
+                                </ListGroupItem>
+                            )}
                         <ListGroupItem>
-                            <Button className='btn-block' type="button" disabled={product.countInStock === 0}>ADD TO CART</Button>
+                            <Button className='btn-block' type="button" disabled={product.countInStock === 0} onClick={addToCartHandler}><AddShoppingCartIcon/> ADD TO CART</Button>
                         </ListGroupItem>
                     </ListGroup>
                 </Col>
             </Row>
         }
+        {product && <Modal show={showModal} onHide={() => setShowModal(false)}>
+            <ModalHeader closeButton>
+            <ModalTitle>Successfully Added To Cart! <CheckCircleOutlineIcon style={{color: "green", marginLeft: ".25em"}}/> </ModalTitle></ModalHeader>
+            <ModalBody>
+            <Row>
+                <Col><Image src={product.image} alt={product.name} fluid style={{maxHeight: "100px"}}/></Col>
+                <Col><h6>{product.name}</h6></Col>
+                <Col><span>Price: <p>${product.price}</p></span></Col>
+                <Col><span>Quantity: <p>{quantity}</p></span></Col>
+            </Row>
+                
+
+            </ModalBody>
+            <ModalFooter>
+            <Button variant="secondary" onClick={() => setShowModal(false)}>Close</Button>
+            <Button variant="primary" onClick={() => navigate("/cart")}>Go To Cart</Button>
+            </ModalFooter>
+        </Modal>}
     </>
   )
 }
