b32d9c969464861b8beb2e5256f6afc4e070f98f
paoloBugarin <paolopaolobugarin@gmail.com>
2022-11-06 00:59:01 -0700

    created loader and message component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import axios from "axios";
 import useProductsRedux from "../hooks/useProductsRedux";
-
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 const HomePage = () => {
   const { productsList } = useProductsRedux();
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
-        {productsList.error && <p>{productsList.error}</p>}
-        {productsList.isLoading && <p>Loading data...</p>}
+        {productsList.error && <Message variant="danger">Error: {productsList.error}</Message>}
+        {productsList.isLoading && <Loader/>}
         {!productsList.isLoading && productsList.products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
             <Product product={product} />
