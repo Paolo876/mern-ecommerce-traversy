83b83874874c9696d101f2e33d8ad991c27652d7
paoloBugarin <paolopaolobugarin@gmail.com>
2022-11-08 00:13:49 -0800

    updated CartPage functions

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -13,10 +13,10 @@ const fetchProductInformations = async ( cartItems, products ) => {
   for (const item of cartItems) {
     const product = products.find(product => product._id === item._id)
     if(product) {
-      _cartItems.push(product)
+      _cartItems.push({...product, quantity: item.quantity})
     } else {
       const res = await axios.get(`http://localhost:3001/api/products/${item._id}`);
-      if(res && res.data) _cartItems.push(res.data)
+      if(res && res.data) _cartItems.push({...res.data, quantity: item.quantity})
     }
   }
   return _cartItems;
@@ -33,11 +33,23 @@ const CartPage = () => {
       }
     }
   }, [products, cartItems])
-  
-  const handleCheckout = () => {
-    console.log(cartItems);
+
+  const handleChangeQuantity = ( payload ) => {
+    changeCartItemQuantity(payload);  //redux dispatch
+    setUpdatedCartItems(prevState => {
+      const updatedItems = [ ...prevState]
+      updatedItems.find(item => item._id === payload._id).quantity = payload.quantity
+      return updatedItems;
+    })
   }
-  
+
+  const handleRemoveFromCart = (id) => {
+    removeFromCart(id) //redux dispatch
+    setUpdatedCartItems(prevState => prevState.filter(item => item._id !== id))
+  }
+  const handleCheckout = () => {
+    console.log(cartItems);   //use cartItems, --only grab id and quantity
+  }
   return (
     <Row>
       <Col md={8}>
@@ -55,13 +67,13 @@ const CartPage = () => {
                   <Col md={3}><Link to={`/product/${item._id}`}>{item.name}</Link></Col>
                   <Col md={2}>${item.price}</Col>
                   <Col md={2}>                                            
-                    <FormControl as="select" value={item.quantity} onChange={e => changeCartItemQuantity({...item, quantity: e.target.value})}>
+                    <FormControl as="select" value={item.quantity} onChange={e => handleChangeQuantity({...item, quantity: e.target.value})}>
                       {[...Array(item.countInStock).keys()].map(item => (
                           <option key={item + 1} value={item + 1}>{item + 1}</option>
                       ))}
                     </FormControl>
                   </Col>
-                  <Col md={2}><Button type="button" variant="light" onClick={() => removeFromCart(item._id)}><DeleteIcon style={{margin: 0}}/></Button></Col>
+                  <Col md={2}><Button type="button" variant="light" onClick={() => handleRemoveFromCart(item._id)}><DeleteIcon style={{margin: 0}}/></Button></Col>
                 </Row>
               </ListGroupItem>
             ))}
