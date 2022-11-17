4001499fd84b885bd3825e79141a7cc3b466a3ee
paoloBugarin <paolopaolobugarin@gmail.com>
2022-11-16 21:55:10 -0800

    updated OrderPage

--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -1,16 +1,35 @@
-import { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
-import useOrderRedux from '../hooks/useOrderRedux';
+import { useEffect, useState } from 'react'
+import { useLocation, useParams } from 'react-router-dom'
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import axios from 'axios';
 
 const OrderPage = () => {
-  const location = useLocation();
-  const { order: { createdOrder } } = useOrderRedux();
-  useEffect(() => {
+  const { state: locationState} = useLocation();
+  const params = useParams();
+  const [ order, setOrder ] = useState(null);
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ error, setError ] = useState(null);
 
+  useEffect(() => {
+    setIsLoading(true)
+    axios.get(`http://localhost:3001/api/orders/${params.id}`, {withCredentials: true})
+    .then(res => {
+      setOrder(res.data)
+      setIsLoading(false)
+    })
+    .catch(err => {
+      setError(err.message)
+      setIsLoading(false)
+    })
   }, [])
-  console.log(createdOrder);
+  
+  if(isLoading) return <Loader/>
+
   return (
-    <div>OrderPage</div>
+    <div>
+
+    </div>
   )
 }
 
