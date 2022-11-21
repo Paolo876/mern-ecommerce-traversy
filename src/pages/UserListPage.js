6907531f432c83c99b522cda56c2d39af357e829
paoloBugarin <paolopaolobugarin@gmail.com>
2022-11-21 00:08:23 -0800

    updated userListPage

--- src/pages/UserListPage.js
+++ src/pages/UserListPage.js
@@ -6,11 +6,16 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import useUserRedux from '../hooks/useUserRedux'
 import axios from 'axios'
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const UserListPage = () => {
   const navigate = useNavigate();
   const { user: { userData } } = useUserRedux();
-
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ error, setError ] = useState(null);
+  const [ users, setUsers ] = useState([])
   // if not logged in, redirect to /login
   useEffect(() => {
     if(!userData) navigate("/login")
@@ -19,11 +24,51 @@ const UserListPage = () => {
 
   //fetch users
   useEffect(()=> {
+    setIsLoading(true)
     axios.get("http://localhost:3001/api/admin/get-users", { withCredentials: true})
-      .then(res => console.log(res.data))
+      .then(res => {
+        setUsers(res.data)
+        setIsLoading(false)
+      })
+      .catch(err => {
+        setError(err.message)
+        setIsLoading(false)
+      })
   }, [])
+
+  if(isLoading) return <Loader/>
+  if(error) return <Message variant="danger">{error.message}</Message>
   return (
-    <div>UserListPage</div>
+    <>
+        <h1>Users</h1>
+        <Table striped bordered hover responsive className='table-sm'>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NAME</th>
+                    <th>EMAIL</th>
+                    <th>isADMIN</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map(item => (
+                    <tr key={item._id}>
+                        <th>{item._id}</th>
+                        <th>{item.name}</th>
+                        <th><a href={`mailto: ${item.email}`} target="_blank" rel="noreferrer" >{item.email}</a></th>
+                        <th>{item.isAdmin ? <CheckCircleOutlineIcon/> : "-" }</th>
+                        <th>
+                            <LinkContainer to={`/user/${item._id}/edit`}>
+                                <Button variant='light' className='btn-sm'><EditIcon style={{margin: "0"}}/></Button>
+                            </LinkContainer>
+                            <Button variant='danger' className='btn-sm'><DeleteIcon style={{margin: "0"}}/></Button>
+                        </th>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    </>
   )
 }
 
