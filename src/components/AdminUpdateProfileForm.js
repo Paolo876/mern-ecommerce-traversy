a2438ddb44becc452a8fb68ed05164f9d46c5496
paoloBugarin <paolopaolobugarin@gmail.com>
2022-11-24 00:44:52 -0800

    updated adminupdateprofileform page

--- src/components/AdminUpdateProfileForm.js
+++ src/components/AdminUpdateProfileForm.js
@@ -1,31 +1,47 @@
 import React, { useState } from 'react'
-import { Form, Button, Row, Col, FormGroup, FormLabel, FormControl } from 'react-bootstrap'
-import useUserRedux from '../hooks/useUserRedux'
+import { Form, Button, Row, Col, FormGroup, FormLabel, FormControl, FormCheck } from 'react-bootstrap'
 import Loader from './Loader'
 import Message from './Message'
+import axios from "axios"
 
 const AdminUpdateProfileForm = ({ user }) => {
   const [ name, setName ] = useState(user.name);
   const [ isAdmin, setIsAdmin ] = useState(user.isAdmin);
   const [ error, setError ] = useState(null)
   const [ isLoading, setIsLoading ] = useState(false)
-  const [ success, setSuccess ] = useState(null);
-
+  const [ success, setSuccess ] = useState(false);
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    setIsLoading(true)
+    axios.put(`http://localhost:3001/api/admin/users/${user._id}/update`, { name, isAdmin }, { withCredentials: true})
+      .then(res => {
+        setName(res.data.name)
+        setIsAdmin(res.data.isAdmin)
+        setIsLoading(false)
+      })
+      .catch(err => {
+        setError(err.message)
+        setIsLoading(false)
+      })
+    setSuccess(true)
   }
 
   if(isLoading) return <Loader/>
   if(error) return <Message variant="danger">{error}</Message>
   return (
     <Col md={4} className="mt-5">
-        <h2>UPDATE PROFILE</h2>
-        {success && <Message variant ="success">Successfully updated profile!</Message>}
+        <h2>UPDATE USER'S PROFILE</h2>
+        {success && <Message variant ="success" dismissible onClose={() => setSuccess(false)}>Successfully updated profile!</Message>}
         <Form onSubmit={handleSubmit}>
             <FormGroup controlId='name' className="my-3">
-                <FormLabel>Enter new name: </FormLabel>
+                <FormLabel><strong>Enter new name:</strong></FormLabel>
                 <FormControl type="text" placeholder={user.name} value={name} onChange={e => setName(e.target.value)} autoComplete="name"/>
+            </FormGroup>
+            <FormGroup controlId='name' className="my-3">
+                <FormLabel className='me-5 mt-3'><strong>Set as admin: </strong></FormLabel>
+                <FormCheck inline type="checkbox" value={isAdmin} onChange={() => setIsAdmin(prevState => !prevState)} id={`inline-checkbox-1`} checked={isAdmin}/>
+                {isAdmin && <p><em><small style={{opacity: ".75"}}>(note: If checked, this user will have all admin privileges.)</small></em></p>}
             </FormGroup>
             <div className="d-grid my-5">
                 {!isLoading && <Button type="submit" variant="primary" size='lg'>UPDATE PROFILE</Button>}
