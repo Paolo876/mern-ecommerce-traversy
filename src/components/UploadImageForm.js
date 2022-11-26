d97f5bad990c0141d5c2c1ddce30dadf70ecc545
paoloBugarin <paolopaolobugarin@gmail.com>
2022-11-25 18:39:42 -0800

    updated uploadimageform

--- src/components/UploadImageForm.js
+++ src/components/UploadImageForm.js
@@ -1,14 +1,23 @@
-import { useState, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { FormGroup, FormControl, FormLabel, Image, Button } from 'react-bootstrap'
+import Message from './Message';
 
 const UploadImageForm = ({ image, setImage, imageData, setImageData }) => {
+  const [ imageError, setImageError] = useState(null);
+
   const inputRef = useRef();
   const onChangeImage = (e) => {
     if (e.target.files[0]) {
-      setImageData(e.target.files[0]);
-      const reader = new FileReader();
-      reader.addEventListener("load", () => setImage(reader.result));
-      reader.readAsDataURL(e.target.files[0]);
+      if(e.target.files[0].size < 1100000){
+        setImageData(e.target.files[0]);
+        setImageError(null)
+        const reader = new FileReader();
+        reader.addEventListener("load", () => setImage(reader.result));
+        reader.readAsDataURL(e.target.files[0]);  
+      } else {
+        setImageError("Image size too large. (please choose an image less than 1mb)")
+        handleRemoveImage();
+      }
     }
   }
 
@@ -17,15 +26,16 @@ const UploadImageForm = ({ image, setImage, imageData, setImageData }) => {
     setImage(null)
     inputRef.current.value = "";
   }
-  
+
   return (
     <FormGroup controlId='image' className="my-4">  
       <FormLabel><strong>Primary Image</strong></FormLabel>    
+        {imageError && <Message variant="danger"><small>{imageError}</small></Message>}
         {image && imageData && <div>
           <Button className='float-end mb-1' variant="warning" size="sm" onClick={handleRemoveImage}>Remove Image</Button>
           <Image src={image} alt="preview-image" fluid/>
         </div>}
-        <FormControl type="file" accept="image/*" onChange={e => onChangeImage(e)} autoComplete="image" className="mt-2" ref={inputRef}/>
+        <FormControl type="file" accept="image/*" onChange={e => onChangeImage(e)} autoComplete="image" className="mt-2" ref={inputRef} required/>
     </FormGroup>
   )
 }
