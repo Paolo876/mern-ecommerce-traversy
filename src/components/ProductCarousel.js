4531cc2dd03e7daa816e74e98af29b32dd7c76d2
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-15 00:20:37 -0800

    updated carousel

--- src/components/ProductCarousel.js
+++ src/components/ProductCarousel.js
@@ -5,6 +5,7 @@ import Loader from './Loader'
 import Message from './Message'
 import useProductsRedux from '../hooks/useProductsRedux'
 import currencyFormatter from '../utils/currencyFormatter'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 const ProductCarousel = () => {
   const { productsList:{ showcaseProducts, isLoading, error} } = useProductsRedux();
   const [ showOverlay, setShowOverlay ] = useState(false)
@@ -17,14 +18,11 @@ const ProductCarousel = () => {
             <CarouselItem key={item._id} onMouseEnter={() => setShowOverlay(true)} onMouseLeave={() => setShowOverlay(false)}>
                 <Link to={`/product/${item._id}`}>
                     <Image src={item.bannerImage.url} alt={item.bannerImage.name} className="d-block w-100 hover-shadow"/>
-                    <Carousel.Caption className='carousel-caption text-dark'>
-                        {/* <h2>{item.name} ({currencyFormatter(item.price)})</h2> */}
-                    </Carousel.Caption>
-                    {showOverlay && <div className='position-absolute translate-middle top-50 start-50 h-100 w-100 bg-dark bg-gradient bg-opacity-25'>
+                    {showOverlay && <div className='position-absolute translate-middle top-50 start-50 h-100 w-100 bg-dark bg-gradient bg-opacity-50'>
                     <Container className='text-light text-center p-5' style={{textShadow: "#333 1px 1px 3px"}}>
-                        <h1 className='pt-5 m-5'>{item.name} {!item.hasOptions && `- ${currencyFormatter(item.price)}`}</h1>
-                        <p className='m-5 px-5'>{item.description}</p>
-                        <Button variant="outline-light" size="lg" className='mt-5'>SHOP NOW</Button>
+                        <h1 className='pt-5 m-5' style={{letterSpacing: ".05em", fontSize: "2.2em"}}>{item.name} {!item.hasOptions && `- ${currencyFormatter(item.price)}`}</h1>
+                        <p className='m-5 px-5' style={{letterSpacing: ".01em"}}>{item.description}...</p>
+                        <Button variant="info" size="lg" className='mt-5 px-4 '>CLICK HERE TO SEE MORE <ChevronRightIcon style={{marginRight: 0}}/></Button>
                     </Container>
                 </div>}
                 </Link>
