f8f65e4f6575eb7cd39a6b78b486337e30c3b65e
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-16 01:08:24 -0800

    updated product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,23 +1,23 @@
 import { Card, Image, Carousel, CarouselItem } from "react-bootstrap";
 import Rating from "./Rating";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import productOptionsPrices from "../utils/productOptionsPrices";
 import currencyFormatter from "../utils/currencyFormatter";
 const Product = ({ product }) => {
   const location = useLocation();
-
+  const navigate = useNavigate();
+  const imageStyleProps = { objectFit: "cover", height:"100%", width: "100%", cursor: "pointer" }
+  const handleClick = () => {
+    navigate(`/product/${product._id}`, {state: {from: location.pathname}})
+  }
   return (
     <Card className="my-3 p-3 rounded" style={{height: "30rem"}}>
-      <Link to={`/product/${product._id}`} state={{from: location.pathname}} style={{overflow: "hidden"}}>
-      {/* <Image src={product.image.url} variant="top" fluid style={{objectFit: "cover", height:"100%", width: "100%"}}/> */}
-
       <Carousel variant="dark" interval={null}>
-          <CarouselItem><Image src={product.image.url} variant="top" fluid style={{objectFit: "cover", height:"100%", width: "100%"}}/></CarouselItem>
+          <CarouselItem><Image src={product.image.url} variant="top" fluid style={imageStyleProps} onClick={handleClick}/></CarouselItem>
           {product.additionalImages.map(item => <CarouselItem key={item._id}>
-            <Image src={item.url} variant="top" fluid style={{objectFit: "cover", height:"100%", width: "100%"}}/>
+            <Image src={item.url} variant="top" fluid style={imageStyleProps} onClick={handleClick}/>
           </CarouselItem>)}
       </Carousel>
-      </Link>
 
       <Card.Body>
         <Link to={`/product/${product._id}`} state={{from: location.pathname}}>
