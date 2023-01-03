f396b6a26740e4cf1575345c0ccb3bc977e9df99
paoloBugarin <paolopaolobugarin@gmail.com>
2023-01-02 22:02:46 -0800

    updated cart page ui

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -33,7 +33,6 @@ const CartPage = () => {
       return updatedItems;
     })
   }
-
   /* if product hasOption, the id of the selectedOption is passed  */
   const handleRemoveFromCart = (id) => {  
     removeFromCart(id) //redux dispatch
@@ -63,17 +62,17 @@ const CartPage = () => {
               {updatedCartItems.map(item => (
                 <ListGroupItem key={item.hasOption ? item.selectedOption._id : item._id}>
                   <Row>
-                    <Col md={2}><Image src={item.hasOption ? item.selectedOption.image.thumbnail : item.image.thumbnail} alt={item.hasOption ? item.selectedOption.image.name: item.image.name} fluid rounded></Image></Col>
-                    <Col md={3}><Link to={`/product/${item._id}`} state={{from: "/cart"}}>{item.name} {item.hasOption && `- ${item.selectedOption.name}`}</Link></Col>
-                    <Col md={2}>{item.hasOption ? currencyFormatter(item.selectedOption.price) : currencyFormatter(item.price)}</Col>
-                    <Col md={2}>                                            
+                    <Col md={2} xs={3} className="px-0"><Image src={item.hasOption ? item.selectedOption.image.thumbnail : item.image.thumbnail} alt={item.hasOption ? item.selectedOption.image.name: item.image.name} fluid rounded></Image></Col>
+                    <Col md={3} xs={9} ><Link to={`/product/${item._id}`} state={{from: "/cart"}}>{item.name} {item.hasOption && `- ${item.selectedOption.name}`}</Link></Col>
+                    <Col md={{span: 2, offset: 1}} xs={{span: 3, offset: 5}}>{item.hasOption ? currencyFormatter(item.selectedOption.price) : currencyFormatter(item.price)}</Col>
+                    <Col md={2} xs={2}>                                            
                       <FormControl as="select" value={item.quantity} onChange={e => handleChangeQuantity({...item, quantity: e.target.value})}>
                         {[...Array(item.hasOption ? parseInt(item.selectedOption.countInStock) : parseInt(item.countInStock)).keys()].map(item => (
                             <option key={item + 1} value={item + 1}>{item + 1}</option>
                         ))}
                       </FormControl>
                     </Col>
-                    <Col md={2}><Button type="button" variant="light" onClick={() => handleRemoveFromCart(item.hasOption ? item.selectedOption._id : item._id)}><DeleteIcon style={{margin: 0}}/></Button></Col>
+                    <Col md={2} xs={2}><Button type="button" variant="light" onClick={() => handleRemoveFromCart(item.hasOption ? item.selectedOption._id : item._id)}><DeleteIcon style={{margin: 0}}/></Button></Col>
                   </Row>
                 </ListGroupItem>
               ))}
@@ -86,11 +85,13 @@ const CartPage = () => {
           <Card>
             <ListGroup variant='flush'>
               <ListGroupItem>
-                <h2>Subtotal: ({updatedCartItems.reduce(( acc, item) => parseInt(acc) + parseInt(item.quantity), 0)}) Items</h2>
-                ${updatedCartItems.reduce(( acc, item) => parseFloat(acc) + parseInt(item.quantity) * parseFloat(item.price), 0).toFixed(2)}
+                <h2 className='mb-3'>Subtotal: ({updatedCartItems.reduce(( acc, item) => parseInt(acc) + parseInt(item.quantity), 0)}) Items</h2>
+                {currencyFormatter(updatedCartItems.reduce(( acc, item) => parseFloat(acc) + parseInt(item.quantity) * (item.hasOption ? parseFloat(item.selectedOption.price): parseFloat(item.price)), 0))}
+                <br/><small><em>* taxes and shipping costs will be calculated upon checkout.</em></small>
+
               </ListGroupItem>
               <ListGroupItem>
-                <Button type="button" className="btn-block" disabled={updatedCartItems.length === 0} onClick={handleCheckout}>
+                <Button type="button" className="btn-block mt-3" disabled={updatedCartItems.length === 0} onClick={handleCheckout} size="lg">
                   Proceed To Checkout
                 </Button>
               </ListGroupItem>
